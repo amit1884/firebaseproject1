4691f6c36133b8673b8af54d059b7f3c12340606
Amit Raj <49913746+amit1884@users.noreply.github.com>
2020-01-11 09:56:30 +0530

    retriving data from firesore

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,24 @@
+db.collection('guides').get().then(snapshot=>{
+  // console.log(snapshot.docs);
+  setupGuides(snapshot.docs);
+})
+
+
+
+
+//checking auth status
+auth.onAuthStateChanged(user=>{
+  if(user){
+    console.log('user logged in');
+  }
+  else{
+    console.log('user logged out');
+  }
+});
+
+
+
+
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -23,7 +44,30 @@ signupForm.addEventListener('submit', (e) => {
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
   e.preventDefault();
-  auth.signOut().then(() => {
-    console.log('user signed out');
+  auth.signOut();
+});
+
+
+
+
+
+const loginForm=document.querySelector('#login-form');
+loginForm.addEventListener('submit',(e)=>{
+  e.preventDefault();
+
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email,password).then(cred=>{
+
+    console.log(cred.user);
+
+    //closing the login box
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+
+    //clear login form
+    loginForm.reset();
+
   });
 });
--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,27 @@ document.addEventListener('DOMContentLoaded', function() {
   M.Collapsible.init(items);
 
 });
+
+
+//accordian data
+
+const guidelist=document.querySelector('.guides');
+
+const setupGuides=(data)=>{
+ 
+  let html='';
+  data.forEach(doc=>{
+    const guide =doc.data();
+    // console.log(guide);
+    const li =`
+    
+    <li>
+        <div class="collapsible-header grey lighten-4">${guide.title}</div>
+        <div class="collapsible-body white">${guide.content}</div>
+      </li>
+    
+    `;
+    html+=li;
+  });
+  guidelist.innerHTML=html;
+}
